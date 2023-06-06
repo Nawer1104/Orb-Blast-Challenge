0ca36033e5b6018c77a783e79dba4ecb4ee2fe86
Nam Nguyen <nam.ngh2211@mail.com>
2023-06-06 12:26:40 +0700

    version changed + refactor

--- Assets/OrbGameManager.cs
+++ Assets/OrbGameManager.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class OrbGameManager : Singleton<OrbGameManager>
+public class OrbGameManager : MonoBehaviour
 {
+    public static OrbGameManager Instance { get; private set;}
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     public List<Ball> balls;
 
     public void RemoveBall(Ball ball)
